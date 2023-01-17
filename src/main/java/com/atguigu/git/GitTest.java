9e08370b8bc3479a9fd2a8b189b414ea897723c0
LiangHe <heliang0126@163.com>
2023-01-17 17:58:47 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Hliang
+ * @create 2023-01-17 17:56
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
